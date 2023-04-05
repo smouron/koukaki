188bb43fcee9819b79b98a3f67e2e74a71f5895c
Stephane Mouron <stephane.mouron@free.fr>
2023-04-05 10:37:22 +0200

    Modification de l'animation du hero et ajout du flotement du titre. Puis ajout : animation sur les nuages + affichage des sections uniquement après avoir scrollé + rotation des fleurs qui accélèrent quand on scroll + section des Oscars

--- wp-content/themes/foce-child/assets/js/custom-scripts.js
+++ wp-content/themes/foce-child/assets/js/custom-scripts.js
@@ -1,7 +1,3 @@
-//
-// Script pour gérer les boutons + et - des quantités dans le formulaire de commandes
-//
-
 console.log("Démarrage du script !");
 
 // Différer le lancement du script => ne se lance qu'une fois que tout le HTML a été chargé
@@ -16,59 +12,99 @@ if (document.readyState === "complete") {
 function monScript() {
   console.log("HTML prêt !");
 
-  const body = document.querySelector("body");
-  const banner = document.querySelector(".banner");
-  const bannerVideo = document.querySelector(".banner__video");
-  const bannerLogo = document.querySelector(".banner__logo");
-  const place = document.querySelector(".place");
+  let posX = 0;
+  let mouveCloud = false;
 
-  const heightControlBanner = bannerLogo.offsetHeight / 2 - 20;
+  const root = document.documentElement;
+  const place = document.querySelector("#place");
+  const littleCloud = document.querySelector(".place--little_cloud");
+  const bigCloud = document.querySelector(".place--big_cloud");
 
-  console.log(banner);
+  const handleIntersect = function (entries, observer) {
+    entries.forEach(function (e) {
+      // Contrôle si l'élément à observer
+      // est dans le ratio de la zone qui est affichée
+      console.log(e.target);
+      if (e.intersectionRatio > ratio) {
+        elementName = e.target.className;
+        console.log(elementName + " est visible");
+        if (
+          elementName === "story hidden" ||
+          elementName === "studio hidden" ||
+          elementName === "nomination hidden" ||
+          elementName === "site-footer hidden"
+        ) {
+          // On valide la class qui va executer le keyframes d'apparition des sections
+          e.target.classList.add("mouve-up");
+          // On arrête l'observation sur cet élément
+          observer.unobserve(e.target);
+          // On retire la class qui cachait par défaut l'élement
+          e.target.classList.remove("hidden");
+        }
+
+        // Si on a trouvé un des nuages on active l'autorisation de déplacement
+        if (
+          elementName === "place--big_cloud" ||
+          elementName === "place--little_cloud"
+        ) {
+          mouveCloud = true;
+        } else {
+          mouveCloud = false;
+        }
+      }
+    });
+  };
+
+  const ratio = 0.1;
+  // Initialisation de l'option pour la fonction IntersectionObserver
+  // root :
+  // threshold : 0.1 = 10% qui doit être visible de l'élement avant de déclencher l'action
+  const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: ratio,
+  };
+
+  // https://developer.mozilla.org/fr/docs/Web/API/Intersection_Observer_API
+  // https://grafikart.fr/tutoriels/scroll-reveal-1176#autoplay
+  //  Initialisation de la fonction IntersectionObserver
+  const observer = new IntersectionObserver(handleIntersect, options);
+
+  // Activation des éléments que l'on va surveiller
+  observer.observe(document.querySelector(".story"));
+  observer.observe(document.querySelector(".studio"));
+  observer.observe(document.querySelector(".nomination"));
+  observer.observe(document.querySelector(".site-footer"));
+  observer.observe(bigCloud);
+  observer.observe(littleCloud);
+
   console.log(
-    "offsetHeight: " +
-      banner.offsetHeight +
-      " - clientHeight: " +
-      banner.clientHeight +
-      " - heightControlBanner: " +
-      heightControlBanner
+    "La résolution d'écran est: " + screen.width + " x " + screen.height
   );
-  console.log(bannerVideo);
-  console.log(bannerLogo);
 
-  //   Intitialisation position de l'image titre dans le header par rapport au scroll
-  if (window.scrollY < heightControlBanner) {
-    bannerLogo.style.setProperty("--transy", window.scrollY + "px");
-  } else {
-    bannerLogo.style.setProperty("--transy", heightControlBanner + "px");
-  }
-
-  //   Gestion de la position de l'image titre dans le header par rapport au scroll
+  // Contrôle si on scroll sur la fenêtre
   window.addEventListener("scroll", () => {
-    let scrollValue =
-      (window.scrollY + window.innerHeight) / document.body.offsetHeight;
+    // Si on scroll, cela accélère la rotation des fleurs
+    // https://www.toutjavascript.com/reference/ref-window.scrolly.php
+    var vertical = -1;
+    setInterval(function () {
+      if (window.scrollY != vertical) {
+        vertical = window.scrollY;
+        root.style.setProperty("--rotate", "3s");
+      } else {
+        root.style.setProperty("--rotate", "15s");
+      }
+    }, 500);
 
-    console.log(scrollValue * 10);
-
-    if (window.scrollY < heightControlBanner) {
-      bannerLogo.style.setProperty("--transy", window.scrollY + "px");
+    // on ne bouge les nuages que s'ils ont été détécté à l'affichage
+    if (mouveCloud) {
+      posX = Math.round(0 - (window.scrollY - place.offsetTop - 200));
+      console.log("posX: " + posX);
+      if (posX <= 0 && posX > -400) {
+        root.style.setProperty("--posX", posX + "px");
+      }
     } else {
-      bannerLogo.style.setProperty("--transy", heightControlBanner + "px");
+      posX = 0;
     }
   });
-
-  // --rotate
-
-  document.addEventListener(
-    "mouseover",
-    (e) => {
-      // on met l'accent sur la cible de mouseover
-      // console.log("id: " + e.target.id + " - class: " + e.target.className);
-      e.target.style.color = "orange";
-      setTimeout(function () {
-        e.target.style.color = "";
-      }, 600);
-    },
-    false
-  );
 }
--- wp-content/themes/foce-child/front-page.php
+++ wp-content/themes/foce-child/front-page.php
@@ -6,14 +6,18 @@ get_header();
     <main id="primary" class="site-main">
         <!-- LIGNE(S) AJOUTEE(S) -->
         <section class="banner">
-            <video class="banner__video" autoplay="autoplay" muted="" src="<?php echo get_theme_file_uri() . '/assets/video/Studio+Koukaki-vidéo+header+sans+son.mp4'; ?>" loop="infinite"></video>
-            <img class="banner__logo" class="banner__logo" src="<?php echo get_theme_file_uri() . '/assets/images/logo.png'; ?>" alt="Logo fleurs d'oranger & chats errants">
+            <img class="banner__background" src="<?php echo get_theme_file_uri() . '/assets/images/banner.png'; ?>" alt="">
+            <video class="banner__video" control poster="<?php echo get_theme_file_uri() . '/assets/images/banner.png'; ?>" autoplay="autoplay" muted=""
+                    loop="infinite">
+                    <source src="<?php echo get_theme_file_uri() . '/assets/video/Studio+Koukaki-vidéo+header+sans+son.mp4'; ?>" type="video/mp4">
+            </video>
+            <img class="banner__logo" src="<?php echo get_theme_file_uri() . '/assets/images/logo.png'; ?>" alt="Logo fleurs d'oranger & chats errants">
         </section>
         <!-- =================== -->
-        <section id="#story" class="story">
-            <h2>L'histoire</h2>
-            <!-- LIGNE(S) AJOUTEE(S) -->
-            <span>L'histoire</span>
+
+        <!-- LIGNE(S) AJOUTEE(S) -->
+        <section id="#story" class="story hidden">
+            <h2><span class="hidden">L'histoire</span></h2>
             <!-- =================== -->
             <article id="" class="story__article">
                 <p><?php echo get_theme_mod('story'); ?></p>
@@ -56,8 +60,8 @@ get_header();
             </article>
             <article id="place">
                 <!-- LIGNE(S) AJOUTEE(S) -->
-                <img class="place--big_cloud" src="<?php echo get_theme_file_uri() . '/assets/images/big_cloud.png" alt="Gros nuage qui passe'; ?>">
-                <img class="place--little_cloud" src="<?php echo get_theme_file_uri() . '/assets/images/little_cloud.png" alt="Petit nuage qui passe'; ?>">
+                <img class="place--big_cloud" src="<?php echo get_theme_file_uri() . '/assets/images/big_cloud.png'; ?>" alt="Gros nuage qui passe">
+                <img class="place--little_cloud" src="<?php echo get_theme_file_uri() . '/assets/images/little_cloud.png'; ?>" alt="Petit nuage qui passe">
                 <!-- =================== -->
                 <div>
                     <h3>Le Lieu</h3>
@@ -68,16 +72,22 @@ get_header();
         </section>
 
 
-        <section id="studio" class="studio">
-            <h2>Studio Koukaki</h2>
-            <!-- LIGNE(S) AJOUTEE(S) -->
-            <span>Studio Koukaki</span>
+        <!-- LIGNE(S) AJOUTEE(S) -->
+        <section id="studio" class="studio hidden">
+            <h2><span class="hidden">Studio Koukaki</span></h2>
             <!-- =================== -->
             <div>
                 <p>Acteur majeur de l’animation, Koukaki est un studio intégré fondé en 2012 qui créé, produit et distribue des programmes originaux dans plus de 190 pays pour les enfants et les adultes. Nous avons deux sections en activité : le long métrage et le court métrage. Nous développons des films fantastiques, principalement autour de la culture de notre pays natal, le Japon.</p>
                 <p>Avec une créativité et une capacité d’innovation mondialement reconnues, une expertise éditoriale et commerciale à la pointe de son industrie, le Studio Koukaki se positionne comme un acteur incontournable dans un marché en forte croissance. Koukaki construit chaque année de véritables succès et capitalise sur de puissantes marques historiques. Cette année, il vous présente “Fleurs d’oranger et chats errants”.</p>
             </div>
-            </section>
+        </section>
+
+        <!-- LIGNE(S) AJOUTEE(S) -->
+        <section id="#nomination" class="nomination">
+            <h3>Fleurs d’oranger & chats errants est nominé aux Oscars Short Film Animated de 2022 !</h3>
+            <img class="nomination__logo" src="<?php echo get_theme_file_uri() . '/assets/images/oscars-2021.png'; ?>" alt="Nomination aux Oscars 2022" ">
+        </section>
+        <!-- =================== -->
     </main><!-- #main -->
 
 <?php
